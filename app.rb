7a9d07e16c0f3548e8c46e0373f1557ca4732e13
korolvs <korolvs@gmail.com>
2016-05-22 19:35:58 +0300

    Application

--- app.rb
+++ app.rb
@@ -1,3 +1,29 @@
 require_relative 'lib/compass'
+require_relative 'lib/plateau'
+require_relative 'lib/state'
+require_relative 'lib/coordinate'
+require_relative 'lib/rover'
 
-puts 'hi'
+max_x, max_y = gets.strip.split(' ').map {|i| i.to_i}
+
+result = []
+line = gets.strip
+while line != ''
+  x, y, direction = line.strip.split(' ')
+  x, y = x.to_i, y.to_i
+
+  plateau = Plateau.new(max_x, max_y)
+  state = State.new(Coordinate.new(x, y), direction)
+  rover = Rover.new(plateau, state)
+
+  commands = gets.strip.split('')
+  commands.each do |command|
+    rover.command(command)
+  end
+  result.push(rover)
+  line = gets.strip
+end
+
+result.each do |rover|
+  puts "#{rover.state.coordinate.x} #{rover.state.coordinate.y} #{rover.state.direction}"
+end
