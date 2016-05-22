f7b83dc282d6c445872cede0a2dc64dcd3325d17
korolvs <korolvs@gmail.com>
2016-05-22 17:30:18 +0300

    Plateau implementation

--- lib/plateau.rb
+++ lib/plateau.rb
@@ -1,6 +1,13 @@
 require_relative 'errors'
-require_relative 'compass'
 require_relative 'coordinate'
 
 class Plateau
+  attr_reader :max_x, :max_y
+
+  def initialize(max_x, max_y)
+    unless max_x.is_a?(Integer) && max_y.is_a?(Integer) && max_x >= 0 && max_y >= 0
+      raise MarsMissionError::NotValidCoordinateError
+    end
+    @max_x, @max_y = max_x, max_y
+  end
 end
