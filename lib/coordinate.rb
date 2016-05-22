c2b63c3550f3f889a4188dac2de6a7c02e944e91
korolvs <korolvs@gmail.com>
2016-05-22 17:18:06 +0300

    Coordinate implementation

--- lib/coordinate.rb
+++ lib/coordinate.rb
@@ -2,4 +2,12 @@ require_relative 'compass'
 require_relative 'errors'
 
 class Coordinate
+  attr_reader :x, :y, :d
+
+  def initialize(x, y, d)
+    unless x.is_a?(Integer) && y.is_a?(Integer) && x >= 0 && y >= 0 && Compass::POINTS.include?(d)
+      raise MarsMissionError::NotValidCoordinateError
+    end
+    @x, @y, @d = x, y, d
+  end
 end
