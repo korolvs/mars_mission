b04cc67eb82a70791dfd4fa200db6c0659262e44
korolvs <korolvs@gmail.com>
2016-05-22 18:06:06 +0300

    Rover creating implementation

--- lib/rover.rb
+++ lib/rover.rb
@@ -4,4 +4,10 @@ require_relative 'plateau'
 require_relative 'errors'
 
 class Rover
+  attr_reader :plateau, :state
+
+  def initialize(plateau, state)
+    raise MarsMissionError::RoverOutOfPlateauError unless plateau.isStateInPlateau(state)
+    @plateau, @state = plateau, state
+  end
 end
